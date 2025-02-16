2371ab793b18f85e29c03c7e2972960470473b29
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2025-02-16 21:56:05 +0000

    runs the listener for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use roxy::cli::console::out_starting_message;
+use roxy::proxy::proxy::listen_to_all_traffic_on;
+// use roxy::client::simple_client::send_get_every_5_seconds_to_port;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    out_starting_message();
+
+    // Start the proxy server thread
+    let proxy_thread = thread::spawn(move || listen_to_all_traffic_on(80));
+
+    // Start a client thread for testing, not yet implemented correctly
+    // let client_thread = thread::spawn(move || send_get_every_5_seconds_to_port(80));
+
+    // (they won't finish, since they run indefinitely)
+    proxy_thread.join().unwrap();
+    // client_thread.join().unwrap();
 }
